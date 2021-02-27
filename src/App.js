cc3f15e23be6e7f04a3f15d593312ae57e6cd37d
RianIslam <rianislamdr@gmail.com>
2021-02-28 01:56:25 +0600

    create api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import userdata from './data.json'
 
 function App() {
+
+  const [user,setUser] = useState([]);
+
+  useEffect(() =>{
+    setUser(userdata);
+    console.log(userdata);
+  }, [])
   return (
     <div className="App">
+    <h1>User profile</h1>
 
     </div>
   );
